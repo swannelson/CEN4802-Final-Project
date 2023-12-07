034c489960f848574c98b0699d40e535eca5aaf6
swannelson <133827181+swannelson@users.noreply.github.com>
2023-12-06 22:39:52 -0500

    added number class

---
+++ eclipse-workspace/square/src/square/number.java
@@ -0,0 +1,25 @@
+package square;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.*;
+
+public class number {
+
+	public static void main(String[] args) throws SecurityException, IOException {
+			 int num = 5;
+			 int result = squared(num);
+			 
+		
+			 
+			 
+		}
+	
+	public static  int squared(int num) {
+		return num * num;
+	}
+}
+		
+	
+	
+
