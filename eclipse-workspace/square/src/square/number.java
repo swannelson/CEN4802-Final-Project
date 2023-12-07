83c96ea0f33435527fa31b4b47f55a2347a4b89b
swannelson <133827181+swannelson@users.noreply.github.com>
2023-12-06 22:43:16 -0500

    added new method

--- eclipse-workspace/square/src/square/number.java
+++ eclipse-workspace/square/src/square/number.java
@@ -7,9 +7,12 @@ import java.util.logging.*;
 public class number {
 
 	public static void main(String[] args) throws SecurityException, IOException {
-			 int num = 5;
+			 int num = 70;
 			 int result = squared(num);
-			 
+			 System.out.println(result);
+			 boolean isDivisible = false;
+			 isDivisible = isDivisibleBy10(num);
+			 System.out.println(isDivisible);
 		
 			 
 			 
@@ -17,6 +20,14 @@ public class number {
 	
 	public static  int squared(int num) {
 		return num * num;
+	}
+	
+	public static boolean isDivisibleBy10(int num) {
+		if(num % 10 == 0)
+		{
+			return true;
+		}
+		return false;
 	}
 }
 		
